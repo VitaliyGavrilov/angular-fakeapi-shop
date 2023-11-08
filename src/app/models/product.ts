dd151a6ed632e6dbc2a3626c30de91f75096ea6a
VitaliyGavrilov <vitaliy140798gavrilov@mail.ru>
2023-11-08 14:55:09 +0300

    подготовили данные

---
+++ src/app/models/product.ts
@@ -0,0 +1,13 @@
+//Создали интерфейс(тип данных) для проуктов
+export interface IProduct {
+  id: number
+  title: string
+  price: number
+  description: string
+  category: string
+  image: string
+  rating: {
+    rate: number
+    count: number
+  }
+}
